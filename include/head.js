02bc905ff69ba366c0a1eff1d73155dc63c41293
Lim JeoungGyu <limjungkoro@gamil.com>
2022-04-06 10:10:16 +0900

    초기파일

---
+++ include/head.js
@@ -0,0 +1,30 @@
+var str = '';
+
+str += '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+str += '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">';
+str += '<meta name="theme-color" content="#000000">';
+str += '<meta name="format-detection" content="telephone=no">';
+str += '<link rel="icon" type="image/png" sizes="192x192" href="../images/android-icon-192x192.png">';
+str += '<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">';
+str += '<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">';
+str += '<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">';
+str += '<meta name="description" content="사이트 소개 320자 이내로 작성.">';
+str += '<meta name="keywords" content="키워드입력, 키워드2, 키워드3">';
+str += '<meta name="author" content="사이트명">';
+str += '<meta property="og:type" content="website">';
+str += '<meta property="og:title" content="해당 링크의 제목">';
+str += '<meta property="og:description" content="해당 링크 설명">';
+str += '<meta property="og:image" content="썸네일 이미지, http:// 부터 시작하는 절대 주소를 써야합니다.">';
+str += '<meta property="og:url" content="링크 주소">';
+str += '<meta name="twitter:card" content="photo">';
+str += '<meta name="twitter:url" content="사이트 주소">';
+str += '<meta name="twitter:description" content="사이트 소개.">';
+str += '<meta name="twitter:image" content="썸네일 이미지, http:// 부터 시작하는 절대 주소를 써야합니다.">';
+str += '<link rel="stylesheet" href="../css/base.css">';
+// str += '<link rel="stylesheet" href="../css/style.css">';
+// str += '<link rel="stylesheet" href="../css/mobile.css">';
+str += '<script src="http://code.jquery.com/jquery-latest.js"></script>';
+str += '<script src="../js/ui.js"></script>';
+
+
+document.write(str);
