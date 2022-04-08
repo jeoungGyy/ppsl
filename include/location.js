c6611ebf69506045692f35ab68cf5ec22a54db17
Lim JeoungGyu <limjungkoro@gamil.com>
2022-04-08 15:41:17 +0900

    .

---
+++ include/location.js
@@ -0,0 +1,15 @@
+var str = '';
+
+str += '<div class="location">';
+str += '  <i class="home"></i>';
+str += '  <div class="locationDepth">';
+str += '    <a href="#" class="text"><span>1뎁스</span></a>';
+str += '    <a href="#" class="text"><span>2뎁스</span></a>';
+str += '  </div>';
+str += '  <button type="button" class="printBtn"><i>화면 인쇄</i></button>';
+str += '  <button type="button" class="shareBtn"><i>페이지 공유</i></button>';
+str += '</div>';
+
+
+document.write(str);
+
--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
 	common = {
 		init: function () {
+			common.initial();
 			common.groupBox();
 			common.menu();
 			// common.menu_active();
@@ -9,6 +10,24 @@ $(document).ready(function () {
 			// common.aside_scroll();
 			// common.layer_popup();
 			// common.family_site();
+		},
+    /* 초기화 */
+		initial: function () {
+      var fired = [];
+      var $controlBox = $(".searchBody .controlBox");
+
+      $(window).on('resize', function () {
+        if ($(this).width() <= 767 && !fired[0]) {
+          fired[0] = true;
+          fired[1] = false;
+        }
+        else if ($(this).width() >= 768 && !fired[1]) {
+          fired[0] = false;
+          fired[1] = true;
+          
+          $controlBox.removeAttr('style');
+        }
+      });
 		},
     /* 메뉴 */
 		groupBox: function () {
@@ -21,11 +40,24 @@ $(document).ready(function () {
 		/* 메뉴 */
 		menu: function () {
 			var $gnb = $(".gnb");
-			var $subMenuBlind = $(".subMenuBlind");
-			var $depth2 = $(".depth2");
+			var $header = $(".header");
+			var $depth1 = $(".gnb .depth1");
+			var $depth2 = $(".gnb .depth2");
+      var $subMenuBlind = $(".subMenuBlind");
       var menu_change = Boolean;
 
       $gnb.on({
+        "mouseenter": function () {
+          setTimeout(menuchk, 300);
+          menu_change = true;
+        },
+        "mouseleave": function () {
+          setTimeout(menuchk, 300);
+          menu_change = false;
+        }
+      });
+
+      $subMenuBlind.on({
         "mouseenter": function () {
           setTimeout(menuchk, 100);
           menu_change = true;
@@ -42,19 +74,28 @@ $(document).ready(function () {
             var _menu_height = $depth2.map(function () {
               return $(this).height();
             }).get();
-            // var _menu_height_value = Math.max.apply(null, _menu_height);
+            var _menu_height_value = Math.max.apply(null, _menu_height);
             // TweenMax.to($gnb_blind, 0.2, { height: _menu_height_value + "", opacity: 1, ease: "Power4.ease" });
             // $gnb_menu_sub.css({"height": _menu_height_value, "opacity": 1});
-            console.log(_menu_height)
+            $header.height(_menu_height_value + 104);
+
+            $depth1.addClass('active');
+            setTimeout(function () {
+              $depth1.addClass('show');
+              $depth2.height(_menu_height_value);
+              $subMenuBlind.height(_menu_height_value);
+            }, 10);
           }, 10);
 
           // $gnb_menu.show().addClass("active");
           // $blind_common.addClass("active");
         } else {
-          // $gnb_menu.hide().removeClass("active");
-          // $blind_common.removeClass("active");
-          // TweenMax.to($gnb_blind, 0.8, { height: 0, opacity: 0, ease: "Expo.easeOut" });
-          // $gnb_menu_sub.css({"height": "", "opacity": 0});
+          $header.height('');
+          $subMenuBlind.height('');
+          $depth1.removeClass('show');
+          setTimeout(function () {
+            $depth1.removeClass('active');
+          }, 10);
         }
       }
 		},
@@ -62,3 +103,20 @@ $(document).ready(function () {
 
 	common.init();
 });
+
+
+/* 알림 */
+inform = {
+  toggleBtn: function () {
+    var $controlBox = $(".searchBody .controlBox");
+    var _this = $(event.currentTarget);
+
+    if($controlBox.css('display') === "none") {
+      $controlBox.slideDown(200);
+      _this.addClass('active').attr({'title': '확장됨'});
+    } else {
+      $controlBox.slideUp(200);
+      _this.removeClass('active').attr({'title': '축소됨'});
+    }
+  }
+}
