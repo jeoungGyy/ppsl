6c633a8dfb491bd60a271d8cb45913c8edf521e4
Lim JeoungGyu <limjungkoro@gamil.com>
2022-04-08 10:47:40 +0900

    알림 등록

---
+++ include/evaluation.js
@@ -0,0 +1,48 @@
+var str = '';
+
+str += '<div class="evaluationBody">';
+str += '  <div class="context">';
+str += '    <p>페이지의 내용이나 사용편의성에 대해 만족하십니까?</p>';
+str += '    <ul class="groupBox radiogroup">';
+str += '      <li>';
+str += '        <div class="radioBox">';
+str += '          <input type="radio" id="radio1" name="radio" checked>';
+str += '          <label for="radio1" class="">매우만족</label>';
+str += '        </div>';
+str += '      </li>';
+str += '      <li>';
+str += '        <div class="radioBox">';
+str += '          <input type="radio" id="radio2" name="radio">';
+str += '          <label for="radio2" class="">만족</label>';
+str += '        </div>';
+str += '      </li>';
+str += '      <li>';
+str += '        <div class="radioBox">';
+str += '          <input type="radio" id="radio3" name="radio">';
+str += '          <label for="radio3" class="">보통</label>';
+str += '        </div>';
+str += '      </li>';
+str += '      <li>';
+str += '        <div class="radioBox">';
+str += '          <input type="radio" id="radio4" name="radio">';
+str += '          <label for="radio4" class="">불만족</label>';
+str += '        </div>';
+str += '      </li>';
+str += '      <li>';
+str += '        <div class="radioBox checked">';
+str += '          <input type="radio" id="radio5" name="radio">';
+str += '          <label for="radio5" class="">매우 불만족</label>';
+str += '        </div>';
+str += '      </li>';
+str += '    </ul>';
+str += '  </div>';
+str += '  <div class="btn">';
+str += '    <button type="button" class="button h40 blue">확인</button>';
+str += '  </div>';
+str += '</div>';
+
+
+document.write(str);
+
+
+
--- include/head.js
+++ include/head.js
@@ -1,27 +1,23 @@
 var str = '';
 
+str += '<title>공적연금연계제도</title>';
 str += '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
 str += '<meta name="viewport" content="width=device-width, viewport-fit=cover">';
 str += '<meta name="theme-color" content="#000000">';
 str += '<meta name="format-detection" content="telephone=no">';
-str += '<link rel="icon" type="image/png" sizes="192x192" href="../images/android-icon-192x192.png">';
-str += '<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">';
-str += '<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">';
-str += '<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">';
-str += '<meta name="description" content="사이트 소개 320자 이내로 작성.">';
-str += '<meta name="keywords" content="키워드입력, 키워드2, 키워드3">';
-str += '<meta name="author" content="사이트명">';
+str += '<link rel="icon" type="image/x-icon" sizes="16x16" href="../../images/common/url_icon.ico">';
+str += '<meta name="description" content="공적연금연계제도 홈페이지">';
+str += '<meta name="keywords" content="공적연금연계제도">';
+str += '<meta name="copyright" content="(우)110-793 서울 종로구 을곡로 75(계동 140-2) 보건복지콜센터 120, 영상전화 : 070 - 7947 - 3745, 청소년전화 1388 ">';
+str += '<meta name="author" content="공적연금연계제도">';
 str += '<meta property="og:type" content="website">';
-str += '<meta property="og:title" content="해당 링크의 제목">';
-str += '<meta property="og:description" content="해당 링크 설명">';
-str += '<meta property="og:image" content="썸네일 이미지, http:// 부터 시작하는 절대 주소를 써야합니다.">';
-str += '<meta property="og:url" content="링크 주소">';
-str += '<meta name="twitter:card" content="photo">';
-str += '<meta name="twitter:url" content="사이트 주소">';
-str += '<meta name="twitter:description" content="사이트 소개.">';
-str += '<meta name="twitter:image" content="썸네일 이미지, http:// 부터 시작하는 절대 주소를 써야합니다.">';
+str += '<meta property="og:title" content="공적연금연계제도">';
+str += '<meta property="og:description" content="공적연금연계제도 홈페이지">';
+str += '<meta property="og:image" content="http://www.ppsl.or.kr/image/ppsl/por/man/ppslmetaog.jpg">';
+str += '<meta property="og:url" content="http://www.ppsl.or.kr">';
 str += '<link rel="stylesheet" href="../../css/base.css">';
 str += '<link rel="stylesheet" href="../../css/style.css">';
+str += '<link rel="stylesheet" href="../../css/tablet.css">';
 str += '<link rel="stylesheet" href="../../css/mobile.css">';
 str += '<script src="../../js/jquery-1.12.4.min.js"></script>';
 str += '<script src="../../js/icheck.js"></script>';
@@ -29,3 +25,5 @@ str += '<script src="../../js/ui.js"></script>';
 
 
 document.write(str);
+
+
